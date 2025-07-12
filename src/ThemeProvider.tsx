b14111440d93f279cdfe7336fd4bb1b262938f08
David Bento <dbento.dev@gmail.com>
2025-07-12 20:39:15 -0300

    feat: change export ThemeProvider type

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -3,11 +3,11 @@ import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from './styles/theme'
 
-export const ThemeProvider = ({ children }: PropsWithChildren) => (
+const ThemeProvider = ({ children }: PropsWithChildren) => (
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
     {children}
   </MuiThemeProvider>
 )
 
-// export default ThemeProvider
+export default ThemeProvider
