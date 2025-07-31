01b33cf474546a48593740dc120ecd8977333ea9
David Bento <dbento.dev@gmail.com>
2025-07-31 19:09:22 -0300

    feat: add webpack prod config

---
+++ config/webpack.prod.js
@@ -0,0 +1,66 @@
+const { ModuleFederationPlugin } = require('webpack').container
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+const path = require('path')
+const deps = require('../package.json').dependencies
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+    clean: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'utilUi',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './components': './src/components',
+        './ThemeProvider': './src/ThemeProvider'
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom']
+        },
+        'react-router-dom': {
+          singleton: true,
+          requiredVersion: deps['react-router-dom']
+        },
+        '@emotion/react': {
+          singleton: true,
+          requiredVersion: deps['@emotion/react']
+        },
+        '@emotion/styled': {
+          singleton: true,
+          requiredVersion: deps['@emotion/styled']
+        },
+        '@mui/material': {
+          singleton: true,
+          requiredVersion: deps['@mui/material']
+        },
+        '@mui/icons-material': {
+          singleton: true,
+          requiredVersion: deps['@mui/icons-material']
+        },
+        'lodash.debounce': {
+          singleton: true,
+          requiredVersion: deps['lodash.debounce']
+        },
+        recharts: {
+          singleton: true,
+          requiredVersion: deps.recharts
+        }
+      }
+    })
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
